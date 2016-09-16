34415f57efd776f76de701253bda2f6a406760f5
SKYMAC <lty406910111@vip.qq.com>
2016-09-16 18:23:09 +0800

    test

--- app/src/main/java/dream/xqyz/MainActivity.java
+++ app/src/main/java/dream/xqyz/MainActivity.java
@@ -10,6 +10,9 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    class test1{
+
+    }
     class test2{
         
     }
