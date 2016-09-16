05abe17aa51120021ca7b77aba023dc7724def0b
dell <1475409647@qq.com>
2016-09-16 17:49:41 +0800

    test2

--- app/src/main/java/dream/xqyz/MainActivity.java
+++ app/src/main/java/dream/xqyz/MainActivity.java
@@ -10,4 +10,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    class test2{
+        
+    }
 }
